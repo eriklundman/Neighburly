087dab539b3606abb8990b000c0b956928a2cede
jocke <joacim.g.larsson@gmail.com>
2020-04-14 17:30:27 +0200

    testgrej bara

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -8,7 +8,7 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1 2</IonTitle>
+          <IonTitle>Hej hej här ändrar jag //Jocke</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
