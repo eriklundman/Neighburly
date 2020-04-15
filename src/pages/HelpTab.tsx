4b16a8bea7761e0440a7ad5bb3c8715f54e581f0
jocke <joacim.g.larsson@gmail.com>
2020-04-15 13:02:47 +0200

    knapp

--- src/pages/HelpTab.tsx
+++ src/pages/HelpTab.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './HelpTab.css';
 
@@ -12,12 +12,7 @@ const HelpTab: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonButton color="primary" id="knapp">knapp</IonButton>
       </IonContent>
     </IonPage>
   );
