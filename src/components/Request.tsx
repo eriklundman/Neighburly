2aac6d4120fee46b7cbc6638995560e567efedb6
siriwerkelin <siriwerkelin@gmail.com>
2020-05-20 10:40:18 +0200

    rund knapp

--- src/components/Request.tsx
+++ src/components/Request.tsx
@@ -263,6 +263,7 @@ const Request: React.FC<any> = (props) => {
             userRef.uid === props.item.h_id ? (
             <IonButtons slot="end">
               <IonButton
+                shape="round"
                 onClick={() => setShowAlert(true)}
                 color="success"
                 fill="solid"
